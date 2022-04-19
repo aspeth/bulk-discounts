39fc3f9fba91ab0cc8dd31064214cb4869e59d3d
Alex Psitos <apsitos08@gmail.com>
2022-04-19 17:44:00 -0400

    Feat: create update method in admin invoices controller

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -8,4 +8,17 @@ class Admin::InvoicesController < ApplicationController
     @invoice = Invoice.find(params[:id])
   end
 
+  def update
+    @invoice = Invoice.find(params[:id])
+    @invoice.update(invoice_params)
+    @invoice.save
+    redirect_to admin_invoice_path(@invoice)
+  end
+
+  private
+
+    def invoice_params
+      params.require(:invoice).permit(:status, :created_at, :updated_at)
+    end
+
 end
