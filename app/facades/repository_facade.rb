ee0498cce74c912c436a6930346fa1df854b7480
Chris Kjolhede <ckjolhede@gmail.com>
2022-04-20 16:41:33 -0600

    merge

---
+++ app/facades/repository_facade.rb
@@ -0,0 +1,14 @@
+require './app/services/github_service.rb'
+
+class RepositoryFacade
+
+  def self.create_or_error_message
+    json = GitHubService.get_repo_data
+    json[:message].nil? ? create_repo : json
+  end
+  
+  def self.create_repo
+    json = GitHubService.get_repo_data
+    Repository.new(json)
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -91,3 +91,9 @@ end
    config.include FactoryBot::Syntax::Methods
   end
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+end
