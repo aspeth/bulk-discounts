b682a8e8319ecaefef20cdf203b2cb3c76581afb
Andrew Speth <speth.andrew@yahoo.com>
2022-04-20 15:30:56 -0600

    forgot to pull from main, had to do it the hard way

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -2,9 +2,6 @@ class Admin::MerchantsController < ApplicationController
 
   def index
     @merchants = Merchant.all
-    @enabled = @merchants.status_enabled
-    @disabled = @merchants.status_disabled
-    @top_five = @merchants.top_5_by_revenue
   end
 
   def show
@@ -20,15 +17,9 @@ class Admin::MerchantsController < ApplicationController
   end
 
   def create
-    merchant = Merchant.new({ 
-      name: params[:name],
-      created_at: DateTime.now, 
-      updated_at: DateTime.now, 
-      })
-      #status automatically defaults to :disabled / 1
-    if merchant.save
-      redirect_to "/admin/merchants"
-    end
+    Merchant.create(name: params[:name], created_at: DateTime.now, updated_at: DateTime.now)
+    #status automatically defaults to :disabled / 1
+    redirect_to "/admin/merchants"
   end
 
   def update
