4e21615a64724501bfa8bd5e20c51b3e82a9cbbc
Andrew Speth <92954894+aspeth@users.noreply.github.com>
2022-04-21 12:54:09 -0600

    Revert "Api work"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :repo_info, only: [:index, :show, :edit, :new]
-  before_action :contributor_info, only: [:index, :show, :edit, :new]
-
+  
   def find_merchant
     @merchant = Merchant.find(params[:merchant_id])
   end
-
-
+  
+  
   def repo_info
-    facade = RepositoryFacade.new
-    @repo = facade.repo
-  end
-
-  def contributor_info
-    facade = ContributorFacade.new
-    @contributor = facade.very_swag_my_guy
+    @repo = RepositoryFacade.create_or_error_message
   end
 end
