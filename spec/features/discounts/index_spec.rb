ddd6f6864ebb81348a0ff3c63ba596a70be7d9a3
Andrew Speth <speth.andrew@yahoo.com>
2022-04-22 09:20:02 -0600

    test: merchant discounts index has all bulk discounts with attributes and links to show page

---
+++ spec/features/discounts/index_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Discount Index' do
+
+  before do
+    @merch1 = Merchant.create!(name: 'Jeffs Gold Blooms', created_at: Time.now, updated_at: Time.now)
+    @merch2 = Merchant.create!(name: 'Miyazakis Dark Souls', created_at: Time.now, updated_at: Time.now)
+    visit "/merchants/#{@merch1.id}/discounts"
+  end
+
+  it "has all my bulk discounts with their percentages and thresholds, and has links to discount show page" do
+    discount_1 = Discount.create!(percent: 10, threshold: 20, merchant_id: @merch1.id)
+    discount_2 = Discount.create!(percent: 20, threshold: 30, merchant_id: @merch1.id)
+    discount_3 = Discount.create!(percent: 15, threshold: 25, merchant_id: @merch2.id)
+
+    within "#discounts-#{discount_1.id}" do
+      expect(page).to have_content("Discount: 10%")
+      expect(page).to have_content("Threshold: 20")
+    end
+    
+    expect(page).to_not have_content("Discount: 15%")
+    expect(page).to_not have_content("Threshold: 25")
+
+    click_link "Discount #1"
+    expect(current_path).to eq("/merchants/#{@merch1.id}/discounts/#{discount_1.id}")
+  end
+end
+
+#    As a merchant
+# When I visit my merchant dashboard
+# Then I see a link to view all my discounts
+# When I click this link
+# Then I am taken to my bulk discounts index page
+# Where I see all of my bulk discounts including their
+# percentage discount and quantity thresholds
+# And each bulk discount listed includes a link to its show page
