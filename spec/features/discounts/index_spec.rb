987185b3edc16a5e71fb3135c81e6c854a99e262
Andrew Speth <speth.andrew@yahoo.com>
2022-04-22 12:51:05 -0600

    discounts index has a link to delete each discount

--- spec/features/discounts/index_spec.rb
+++ spec/features/discounts/index_spec.rb
@@ -82,4 +82,31 @@ RSpec.describe 'Merchant Discount Index' do
 
     expect(page).to have_content("Error: Please enter a whole number between 1 and 100")
   end
+
+  it "has a link to delete each discount" do
+    merch1 = Merchant.create!(name: 'Jeffs Gold Blooms', created_at: Time.now, updated_at: Time.now)
+    merch2 = Merchant.create!(name: 'Miyazakis Dark Souls', created_at: Time.now, updated_at: Time.now)
+    discount_1 = Discount.create!(percent: 10, threshold: 20, merchant_id: merch1.id)
+    discount_2 = Discount.create!(percent: 20, threshold: 30, merchant_id: merch1.id)
+    discount_3 = Discount.create!(percent: 15, threshold: 25, merchant_id: merch2.id)
+    
+    visit "/merchants/#{merch1.id}/discounts"
+
+    expect(page).to have_content("Percent: 10%")
+    expect(page).to have_content("Threshold: 20")
+
+    within "#discounts-#{discount_1.id}" do
+      click_link "Delete Discount #{discount_1.id}"
+    end
+    
+    expect(current_path).to eq("/merchants/#{merch1.id}/discounts")
+    expect(page).to_not have_content("Percent: 10%")
+    expect(page).to_not have_content("Threshold: 20")
+  end
 end
+# As a merchant
+# When I visit my bulk discounts index
+# Then next to each bulk discount I see a link to delete it
+# When I click this link
+# Then I am redirected back to the bulk discounts index page
+# And I no longer see the discount listed
