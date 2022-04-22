f7c19dd51d545d98fd4c826891848ca2576889f9
Andrew Speth <speth.andrew@yahoo.com>
2022-04-22 14:45:03 -0600

    test: discount show has a link to edit the discount

--- spec/features/discounts/show_spec.rb
+++ spec/features/discounts/show_spec.rb
@@ -19,7 +19,39 @@ it "has the bulk discount's percentage and threshold" do
   expect(page).to have_content("Percent: 10%")
   expect(page).to have_content("Threshold: 20")
   end
+
+  it "has a link to a form to edit the discount" do
+    merch1 = Merchant.create!(name: 'Jeffs Gold Blooms', created_at: Time.now, updated_at: Time.now)
+    merch2 = Merchant.create!(name: 'Miyazakis Dark Souls', created_at: Time.now, updated_at: Time.now)
+    discount_1 = Discount.create!(percent: 10, threshold: 20, merchant_id: merch1.id)
+    discount_2 = Discount.create!(percent: 20, threshold: 30, merchant_id: merch1.id)
+    discount_3 = Discount.create!(percent: 15, threshold: 25, merchant_id: merch2.id)
+    
+    visit "/merchants/#{merch1.id}/discounts"
+
+    expect(page).to_not have_content("Percent: 40%")
+    expect(page).to_not have_content("Threshold: 50")
+    
+    click_link 'Edit Discount'
+    
+    fill_in 'Percent', with: '40'
+    fill_in 'Threshold', with: '50'
+    click_button 'Submit'
+    
+    expect(current_path).to eq("/merchants/#{merch1.id}/discounts")
+    expect(page).to have_content("Percent: 40%")
+    expect(page).to have_content("Threshold: 50")
+    expect(page).to_not have_content("Percent: 10%")
+    expect(page).to_not have_content("Threshold: 20")
+  end
+
 end
 # As a merchant
 # When I visit my bulk discount show page
-# Then I see the bulk discount's quantity threshold and percentage discount
+# Then I see a link to edit the bulk discount
+# When I click this link
+# Then I am taken to a new page with a form to edit the discount
+# And I see that the discounts current attributes are pre-poluated in the form
+# When I change any/all of the information and click submit
+# Then I am redirected to the bulk discount's show page
+# And I see that the discount's attributes have been updated
